0fafa798b10bb57add8918c251ed7ec866981c2a
ikeda042 <ikeda042main@gmail.com>
2024-09-06 18:12:42 +0900

    base

---
+++ base.py
@@ -0,0 +1,9 @@
+from opentrons import protocol_api
+from typing import Literal
+
+metadata = {
+    "protocolName": "Base Protocol",
+    "author": "ikeda042",
+    "description": "A base protocol for OpenTrons.",
+    "apiLevel": "2.18",
+}
