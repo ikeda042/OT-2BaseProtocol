efaa2137f11354d21dbcfd5da9bb5ad08f862ab2
ikeda042 <ikeda042main@gmail.com>
2024-09-06 18:16:52 +0900

    enum

--- base.py
+++ base.py
@@ -12,4 +12,12 @@ metadata = {
 class TipRackType(Enum):
     P20 = "opentrons_96_tiprack_20ul"
     P300 = "opentrons_96_tiprack_300ul"
-    P1000 = "opentrons_96_tiprack_1000ul"
+
+
+class PipetteType(Enum):
+    P20 = "p20_single_gen2"
+    P300 = "p300_single_gen2"
+
+
+class PlateType(Enum):
+    P96 = "corning_96_wellplate_360ul_flat"
