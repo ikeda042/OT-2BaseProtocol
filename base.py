c9e95f691ad892b556d83b3ddc6d453cfa228c44
ikeda042 <ikeda042main@gmail.com>
2024-09-06 18:15:06 +0900

    enum

--- base.py
+++ base.py
@@ -1,5 +1,5 @@
 from opentrons import protocol_api
-from typing import Literal
+from enum import Enum
 
 metadata = {
     "protocolName": "Base Protocol",
@@ -7,3 +7,9 @@ metadata = {
     "description": "A base protocol for OpenTrons.",
     "apiLevel": "2.19",
 }
+
+
+class TipRackType(Enum):
+    P20 = "opentrons_96_tiprack_20ul"
+    P300 = "opentrons_96_tiprack_300ul"
+    P1000 = "opentrons_96_tiprack_1000ul"
